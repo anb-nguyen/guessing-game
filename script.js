e07e5bf623b3a96444fc0da69b5c8546d422b08b
an <anconkhi@gmail.com>
2022-12-13 17:48:37 +0100

    Add css style and function for checkGuess in js

--- script.js
+++ script.js
@@ -5,5 +5,33 @@ const lastResult = document.querySelector(".lastResult");
 const tooHighOrLow = document.querySelector(".tooHighOrLow");
 const myInput = document.querySelector(".myInput");
 const guessSubmit = document.querySelector(".guessSubmit");
+let guessCount = 1;
 
-function checkGuess() {}
+function checkGuess() {
+  const userGuess = Number(myInput.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Previous guesses: ";
+  }
+
+  guesses.textContent += userGuess + " ";
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Congrats, you got it right!";
+
+    lastResult.style.backgroundColor = "green";
+  } else {
+    lastResult.textContent = "Wrong number, guess again";
+    lastResult.style.backgroundColor = "red";
+    if (userGuess > randomNumber) {
+      tooHighOrLow.textContent = "Last guess was too high!";
+    } else if (userGuess < randomNumber) 7;
+    tooHighOrLow.textContent = "Last guess was too low!";
+  }
+
+  guessCount++; //add 1 (increment) when the player uses their turn
+  myInput.value = ""; //clear the submit field
+  myInput.focus(); //focus back to the submit field
+}
+
+guessSubmit.addEventListener("click", checkGuess);
+console.log(randomNumber);
